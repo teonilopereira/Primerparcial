d1e94fe260c05aae9dc0b1cbd64cdd3268d6bfe6
Usuario <Usuario@DESKTOP-8F707LD>
2023-07-11 20:01:58 -0300

    Agregar archivos de proyecto.

---
+++ Primerparcial/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Primerparcial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura, ladobase, areapiramide, volumenpiramide, arista, cantidadpiramide;
+            cantidadpiramide = 0;
+            do
+            {
+
+                Console.WriteLine("Ingrese la altura");
+                altura = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el lado de la base");
+                ladobase = double.Parse(Console.ReadLine());
+                areapiramide = Areadelapiramide(ladobase, altura);
+                arista = Valordearista(ladobase, altura);
+                volumenpiramide = volumendelapiramide(ladobase, altura);
+                cantidadpiramide = cantidadpiramide + 1;
+                Console.WriteLine($"el area de la piramide es {areapiramide} y el volumen es {volumenpiramide}");
+                if (altura == 0) 
+                {  
+                    if (ladobase == 0) { }
+                
+                }
+                
+               
+
+                
+                
+
+
+
+
+            } while (true);
+            Console.WriteLine($"la cantidad de piramides ingresadas es {cantidadpiramide}");
+
+        }
+        private static double volumendelapiramide(double ladobase, double altura)=> (Math.Pow(ladobase,2) * altura) / 3;
+        private static double Valordearista(double ladobase, double altura) => Math.Sqrt(Math.Pow(altura, 2) + Math.Pow(ladobase, 2) / 2);
+        private static double Areadelapiramide(double ladobase, double altura) =>ladobase*(ladobase + Math.Sqrt(4*Math.Pow(altura,2)+Math.Pow(ladobase,2)));
+    }
+}
---
+++ primerparcialform/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace primerparcialform
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(546, 63);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(642, 385);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(278, 202);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
